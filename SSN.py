10d8cc40301fcaf0baac2ff24b10bfd51193ba00
Samuel Charles <samyrd309@gmail.com>
2022-05-17 17:08:51 -0400

    Desarrollo del codigo

--- SSN.py
+++ SSN.py
@@ -1,3 +1,34 @@
 class SSN(object):
-  def FunctionName(args):
-    return 
+  def ssnValidator(ssnInput):
+    blocks = ssnInput.split('-')
+    if len(blocks) == 3 and ssnInput != '':
+      first = blocks[0]
+      second = blocks[1]
+      third = blocks[2]
+      if len(first) == 3 and len(second) == 2 and len(third) == 4:
+        if (first == '000' or first== '666') or (int(first) >= 900 and int(first) <=999):
+          ssnOutput = 'Invalid code. The first part should not be 000, 666, or between 900 and 999'
+          print(ssnOutput)
+          return ssnOutput
+        elif int(second)< 1 or int(second) > 99:
+          ssnOutput = 'Invalid code. The second part should be from 01 to 99'
+          print(ssnOutput)
+          return ssnOutput
+        elif int(third) < 1 or int(third) > 9999:
+          ssnOutput = 'Invalid code. The third part should be from 0001 to 9999'
+          print(ssnOutput)
+          return ssnOutput
+        else:
+          ssnOutput = 'valid SSN code'
+          print(ssnOutput)
+        return ssnOutput
+      else:
+        ssnOutput = 'Invalid length input'
+        print(ssnOutput)
+        return ssnOutput 
+    else:
+      ssnOutput = 'Invalid length input'
+      print(ssnOutput)
+      return ssnOutput
+  ssnInput = input('Insert your code SSN: ')
+  ssnValidator(ssnInput)
--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 import unittest
-import SSN
+from SSN import SSN
+
+class TestSSN(unittest.TestCase):
+  def test_TC0(self):
+    self.assertEqual('Invalid length input', SSN.ssnValidator(''))
+  def test_TC1(self):
+    self.assertEqual('valid SSN code', SSN.ssnValidator('856-45-6789'))
+  def test_TC2(self):
+    self.assertEqual('Invalid code. The third part should be from 0001 to 9999', SSN.ssnValidator('856-45-0000'))
+  def test_TC3(self):
+    self.assertEqual('Invalid code. The first part should not be 000, 666, or between 900 and 999', SSN.ssnValidator('000-45-6789'))
+  
+  
+
 
 
 if __name__ == '__main__':
